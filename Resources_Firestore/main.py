af9779d4b8f4a4b7f8f6d4b6767012975c4ecd53
Your Name <you@example.com>
2020-12-20 18:30:07 +0000

    Subir

---
+++ Resources_Firestore/main.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+from datetime import datetime
+from random import randint
+
+from flask import jsonify
+from google.cloud import firestore
+
+db = firestore.Client()
+
+def events_firestore(request):
+  payload = request.get_json(silent=True)
+  resource_id=payload ['id']
+  resource_name = payload['name']
+  category_id = payload ['categoryId']
+  provider_id = payload ['providerId']
+  promotion = payload ['promotion']
+
+  doc_ref = db.collection('resources').document()
+  doc_ref.set({
+    'resource_id': resource_id,
+    'resource_name': resource_name,
+    'category_id': category_id,
+    'provider_id': provider_id,
+    'promotion' : promotion,
+    
+  })
+
+  return jsonify({ 'ok': 42, 'externalId': doc_ref.id })
+  
